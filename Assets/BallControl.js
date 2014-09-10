d05ade44e618320ace1cac9521e95b8443c504f9
Daniel Sanchez <dsan@mit.edu>
2014-09-10 13:40:25 -0400

    files added

---
+++ Assets/BallControl.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+var ballSpeed: float = 80;
+
+function Start () {
+	yield WaitForSeconds(2);
+	GoBall();
+}
+
+function OnCollisionEnter2D (colInfo : Collision2D) {
+	if (colInfo.collider.tag == "Player") {
+		var velY = rigidbody2D.velocity.y;
+		
+		rigidbody2D.velocity.y = velY/2 + colInfo.collider.rigidbody2D.velocity.y/3;
+	}
+}
+
+function ResetBall() {
+	rigidbody2D.velocity.y = 0;
+	rigidbody2D.velocity.x = 0;
+	transform.position.x = 0;
+	transform.position.y = 0;
+	
+	yield WaitForSeconds(1);
+	GoBall();
+}
+
+function GoBall() {
+	var randomNumber = Random.Range(0, 2);
+	
+	if (randomNumber <= 0.5) {
+		rigidbody2D.AddForce(new Vector2(ballSpeed, 10));
+	} else {
+		rigidbody2D.AddForce(new Vector2(-ballSpeed, -10));
+	}
+}
---
+++ Assets/GameManager.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+static var playerScore_1 : int = 0;
+static var playerScore_2 : int = 0;
+
+var skin : GUISkin;
+
+static function Score (wallName : String) {
+	if (wallName == "rightWall") {
+		playerScore_1 += 1;
+	} else if (wallName == "leftWall") {
+		playerScore_2 += 1;
+	}
+}
+
+function OnGUI() {
+	GUI.skin = skin;
+	GUI.Label(new Rect(Screen.width/2 - 150 - 12, 20, 100, 100), "" + playerScore_1);
+	GUI.Label(new Rect(Screen.width/2 + 150 - 12, 20, 100, 100), "" + playerScore_2);
+}
